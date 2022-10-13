3c034b34d1d6ab795878942b7f56df53d6268fd7
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-13 21:41:03 +0200

    nome piu lungo di 3 caratteri

--- index_snack2.php
+++ index_snack2.php
@@ -4,3 +4,35 @@
         che mail contenga un punto e una chiocciola e che age sia un numero. 
         Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
  -->
+
+
+ <?php
+
+    // $name = "name";
+    // $mail = "mail";
+    // $age = "age";
+
+    // GET parame"tri
+    $_GET ["name"];
+    $_GET ["mail"];
+    $_GET ["age"];
+
+    // echo $_GET ["name"];
+
+    $accessOk = "access granted";
+    $accessDenied = "access denied";
+
+    if((strlen($_GET ["name"]) >= 3) /*&& (strpos($_GET ["mail"], "@", "."))*/) {
+        echo $accessOk;
+    }
+    else {
+        echo $accessDenied;
+    }
+
+?>
+
+<p>
+    <?= strlen($_GET ["name"]) ?>
+</p>
+
+<!-- && strpos($mail, ".","@") && $age !isNan) -->
