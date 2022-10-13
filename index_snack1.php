61a99074eab8c770d0bb1a942a99bce20b033732
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-13 20:43:14 +0200

    game two

--- index_snack1.php
+++ index_snack1.php
@@ -8,20 +8,37 @@
 
  <?php
 
-    // array team and points
+    // array team and points gameOne
     $gameOne =[
         'homeTeam' => 'Los Angeles Lakers',
         'pointsHome' => '113',
         'awayTeam' => 'Golden State Warriors',
         'pointsAway' => '108'
+    ];
+
+
+    // array team and points gameTwo
+     $gameTwo =[
+        'homeTeam' => 'Denver Nuggets',
+        'pointsHome' => '97',
+        'awayTeam' => 'Oklahoma City Thunder',
+        'pointsAway' => '101'
     ]
 
  ?>
 
 
-<!-- game score on web page -->
+<!-- gameOne score on web page -->
 <div>
     <p>
         <?= $gameOne["homeTeam"]?> - <?= $gameOne["awayTeam"]?> | <?= $gameOne["pointsHome"]?> - <?= $gameOne["pointsAway"]?>
+    </p>
+</div>
+
+
+<!-- gameTwo score on web page -->
+<div>
+    <p>
+        <?= $gameTwo["homeTeam"]?> - <?= $gameTwo["awayTeam"]?> | <?= $gameTwo["pointsHome"]?> - <?= $gameTwo["pointsAway"]?>
     </p>
 </div>
