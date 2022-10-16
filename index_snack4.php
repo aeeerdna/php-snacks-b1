49b8fcfba4c9d7ad0c310a9f75ca459189f3ab76
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-16 22:58:49 +0200

    snack4

--- index_snack4.php
+++ index_snack4.php
@@ -3,3 +3,17 @@
         Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
         Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
  -->
+
+<?php
+
+    //  paragraph
+    $paragraph = "Growing up in the Netherlands. with its network of pathways. its flat landscape and its bicycle-friendly traffic laws. the brothers Ties and Taco Carlier were commuting with their parents on bikes by age 4.";
+
+    $array = explode('.', $paragraph);
+
+    echo '<pre>';
+
+    var_dump($array);
+
+    echo '</pre>'
+?>
